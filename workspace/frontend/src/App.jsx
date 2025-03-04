9400ed09efe27f7eb2d7805386f4b34255fbd5ce
Alon Baker <66329736+RealSycoBak@users.noreply.github.com>
2025-03-04 18:32:32 -0500

    first commit

--- workspace/frontend/src/App.jsx
+++ workspace/frontend/src/App.jsx
@@ -1,6 +1,17 @@
+import NavBar from './components/navbar/navbar';
+import Profile from './components/profile/profile';
+import Projects from './components/projects/projects'
+import './App.css'
+
+
 function App() {
     return (
-        <h1>Hello World</h1>
+        <>
+            <NavBar />
+            <Profile />
+            <Projects />
+        </>
+        
     )
 }
 
---
+++ workspace/frontend/src/components/navbar/navbar.jsx
@@ -0,0 +1,15 @@
+
+import './navbar.css'
+
+function NavBar(){
+    return (
+        <div id="navbar">
+            <h1>Alon</h1>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#exp">Experiences</a>
+        </div>
+    )
+}
+
+export default NavBar;
